745b28ebc8c302d655e97e34b28698091cb28a42
Mette Munne <mette.ruusu@gmail.com>
2024-12-08 02:24:13 +0200

    add query-language project and add classes to it

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,36 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(
+        HasAtLeast(5, "goals"),
+        HasAtLeast(20, "assists"),
+        PlaysIn("PHI")
+    )
+
+    matcher1 = And(
+        Not(HasAtLeast(2, "goals")),
+        PlaysIn("NYR")
+    )
+
+    print("Esimerkki")
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("NYR pelaajat, joilla ei ole vähintään kahta maalia")
+    for player in stats.matches(matcher1):
+        print(player)
+        
+    print("Seuraavaksi pitäisi tulostua 958")
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+
+
+
+if __name__ == "__main__":
+    main()
